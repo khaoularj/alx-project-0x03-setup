f0d226f8084eb99ff87009abb8a21eac70660140
khaoularj <khaoula.raja1@gmail.com>
2025-01-13 08:50:38 +0000

    updating index.tsx

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,6 +1,7 @@
+import {PageRouteProps} from  "@/interface";
 import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
-import {PageRouteProps} from  "@/interface";
+
 
 
 export default function Home() {
